e2f98e2c204901b88b0ca65fe7f6a2d30e83b792
Dylan Cano Álvarez <dcanalv@upv.edu.es>
2022-05-10 02:25:54 +0200

    Modificacion de la foto

--- src/poiupv/FXMLDocumentController.java
+++ src/poiupv/FXMLDocumentController.java
@@ -126,7 +126,7 @@ public class FXMLDocumentController implements Initializable {
         initData();
         //==========================================================
         // inicializamos el slider y enlazamos con el zoom
-        zoom_slider.setMin(0.5);
+        zoom_slider.setMin(0.2);
         zoom_slider.setMax(1.5);
         zoom_slider.setValue(1.0);
         zoom_slider.valueProperty().addListener((o, oldVal, newVal) -> zoom((Double) newVal));
